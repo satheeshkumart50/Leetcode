a3d3bee6e06eb552226ee06cde41c1089345dbc9
Sathish Kumar Thiagarajan <112828674+satheeshkumart50@users.noreply.github.com>
2026-06-19 14:17:41 -0600

    Add solution: Two Sum II - Input Array Is Sorted

--- solutions/Array/2-two-sum-ii-input-array-is-sorted.py
+++ solutions/Array/2-two-sum-ii-input-array-is-sorted.py
@@ -2,18 +2,18 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 # Time Complexity - O(n)
 # Space Complexity - O(1)
-# Hint - Use two pointers in opposite direction, increment first_pt if sum of first_pt + last_pt is less than target, decrement last_pt if sum of first_pt + last_pt is greater than target
+# Hint - Use two pointers in opposite direction, increment left_pt if sum of left_pt + right_pt is less than target, decrement right_pt if sum of left_pt + right_pt is greater than target
 
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum_2(self, numbers: List[int], target: int) -> List[int]:
 
-        first_pt = 0
-        last_pt = len(numbers)-1
+        left_pt = 0
+        right_pt = len(numbers) - 1
 
-        while first_pt < last_pt:
-            sum = numbers[first_pt]+numbers[last_pt]
+        while left_pt < right_pt:
+            sum = numbers[left_pt] + numbers[right_pt]
             if sum == target:
-                return [first_pt+1, last_pt+1]
+                return [left_pt + 1, right_pt + 1]
             elif sum < target:
-                first_pt += 1
+                left_pt += 1
             else:
-                last_pt -= 1
+                right_pt -= 1
