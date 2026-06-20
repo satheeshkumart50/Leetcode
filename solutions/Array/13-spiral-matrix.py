eff31889e0c43d15f2e0dbe4f86e52f33e5399fc
Sathish Kumar Thiagarajan <112828674+satheeshkumart50@users.noreply.github.com>
2026-06-20 14:11:37 -0600

    Add solution: Spiral Matrix

--- solutions/Array/13-spiral-matrix.py
+++ solutions/Array/13-spiral-matrix.py
@@ -4,7 +4,7 @@
 # Space Complexity - O(m*n) for the output list and O(1) for the pointers and variables used in the algorithm
 # Category - Array
 # Hint - Use four pointers to keep track of the boundaries of the matrix, left, right, top and bottom, we can iterate through the matrix in a spiral manner by moving the pointers accordingly, we can use a while loop that continues until the length of the output list is less than the total number of elements in the matrix, in each iteration we can move right, down, left and up and update the pointers accordingly, at the end we return the output list which will contain the elements in spiral order
-    # Refer - video - https://www.youtube.com/watch?v=3Zv-s9UUrFM
+# Refer - video - https://www.youtube.com/watch?v=3Zv-s9UUrFM
 
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
 
@@ -17,7 +17,7 @@
             for i in range(left, right + 1):
                 spiral_lst.append(matrix[top][i])
 
-            # important to check the length of spiral_lst after each direction to avoid adding duplicate elements when there is only one row left in the matrix
+            # important to check the length of spiral_lst after each direction to avoid adding duplicate elements when there is only one row left in the matrix for testcase like matrix = [[1, 2, 3, 4]]
             if len(spiral_lst) == (len(matrix) * len(matrix[0])):
                 break
 
@@ -31,7 +31,7 @@
             for k in range(right, left - 1, -1):
                 spiral_lst.append(matrix[bottom][k])
 
-            # important to check the length of spiral_lst after each direction to avoid adding duplicate elements when there is only one column left in the matrix
+            # important to check the length of spiral_lst after each direction to avoid adding duplicate elements when there is only one column left in the matrix for testcase like matrix = [[1], [2], [3], [4]]
             if len(spiral_lst) == (len(matrix) * len(matrix[0])):
                 break
 
